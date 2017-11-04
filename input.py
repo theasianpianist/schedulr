78344aae6cfbf4fc1ac4e8dd406ef033ae7cf2f6
Lawrence Lee <lfqlee314@gmail.com>
2017-11-04 16:59:01 -0400

    add basic database functionality

--- input.py
+++ input.py
@@ -1,4 +1,9 @@
 import re
+import sqlite3 as sql
+import sys
+
+db_name = "test.db"
+con = None
 
 def get_input():
 	num_classes = input("How many classes do you have? ")
@@ -10,11 +15,34 @@ def get_input():
 	schedule = []
 	for i in range(num_classes):
 		course = []
-		course.append(input("Name of class " + str(i + 1) + ": "))
-		course.append(input("Start time of class " + str(i + 1) + ": "))
-		course.append(input("End time of class " + str(i + 1) + ": "))
+		course.append(input("Name of class " + str(i + 1) + ": ").upper())
+		course.append(input("Start time of class " + str(i + 1) + ": ").upper())
+		course.append(input("End time of class " + str(i + 1) + ": ").upper())
 		schedule.append(course)
 	return schedule
 
+def put_db(user, schedule, friends = None):
+	user_sanitized = user.upper()
+	schedule_sanitized = schedule
+	friends_sanitized = []
+	if friends:
+		friends_sanitized = [x.upper() for x in friends]
+	try:
+		con = sql.connect(db_name)
+		with con:
+			cur = con.cursor()
+			cur.execute('''CREATE TABLE IF NOT EXISTS main(email TEXT PRIMARY KEY, classes TEXT, friends TEXT)''')
+			cur.execute('''INSERT INTO main VALUES(?, ?, ?)''', (str(user_sanitized), str(schedule_sanitized), str(friends_sanitized)))
+	except sql.Error as error:
+		print("Error %s", str(error))
+		sys.exit(1)
+	finally:
+		if con:
+			con.close()
+
+
+
+
 if __name__ == "__main__":
-	print(get_input())
+	schd = get_input()
+	put_db("user1", schd)
