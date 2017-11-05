38640e2f566731b83a0b06abdf641ded5fab5f3a
Lawrence Lee <lfqlee314@gmail.com>
2017-11-05 04:57:55 -0500

    fixed some shit

--- app.py
+++ app.py
@@ -1,19 +1,36 @@
 from flask import Flask, g
 from flask import Flask, flash, redirect, render_template, request, session, abort
-from flask_login import login_required, logout_user, login_user, LoginManager
+from flask_login import login_required, logout_user, login_user, LoginManager, current_user, UserMixin, login_manager
 import os
 import database
 from user_management import check_password
 app = Flask(__name__)
 
- 
+lm = LoginManager()
+lm.init_app(app)
+lm.login_view = 'login'
+
+class User(UserMixin):
+	def __init__(self, id):
+		self.id = id
+		self.name = "user" + str(id)
+		self.password = self.name + "_secret"
+
+	def __repr__(self):
+		return "%s/%s/%s" % (self.id, self.name, self.password)
+
+@app.before_request
+def before_request():
+	g.user = current_user
+
 @app.route('/')
 @app.route('/index')
 def index():
-    if not session.get('logged_in'):
-        return render_template('login.html')
-    else:
-        return render_template('index.html', user = g.user)
+	if not session.get('logged_in'):
+		return render_template('login.html')
+	else:
+		return render_template('index.html', user = g.user.get_id())
+
 @app.route('/refresh_index')
 def refresh_index():
 	return render_template('index.html', user="no")
@@ -32,13 +49,14 @@ def submit_classes():
 	print(class1 + class2)
 
  
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['GET','POST'])
 def login():
 	username = str(request.form['username'])
 	password = str(request.form['password'])
 	if check_password(username, password):
 		session['logged_in'] = True
-		g.user = username
+		user = User(username)
+		login_user(user)
 	else:
 		flash("Incorrect Username or Password")
 	return index()
@@ -46,8 +64,12 @@ def login():
 @app.route("/logout")
 def logout():
 	session['logged_in'] = False
-	g.user = None
+	logout_user()
 	return index()
+
+@lm.user_loader
+def load_user(userid):
+    return User(userid)
 
 if __name__ == "__main__":
 	app.secret_key = os.urandom(12)
